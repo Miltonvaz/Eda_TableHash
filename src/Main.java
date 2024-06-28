d8763e23e20e5f73ad076931cdca51b9749bcc11
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-06-28 11:02:25 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import hash.HashTable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        HashTable hashTable1 = new HashTable(1000);
+        HashTable hashTable2 = new HashTable(1000);
+
+        String line;
+        String splitBy = ",";
+        int id = 1;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("bussines.csv"))) {
+            while ((line = br.readLine()) != null) {
+                String[] business = line.split(splitBy);
+                if (business.length >= 5) {
+                    String key = business[1];
+                    String value = "ID=" + business[0] + ", Address=" + business[2] +
+                            ", City=" + business[3] + ", State=" + business[4];
+
+                    long time1 = hashTable1.measurePutTime(key, value, 1);
+                    long time2 = hashTable2.measurePutTime(key, value, 2);
+
+                    System.out.println("[" + id + "] Business [ID=" + business[0] + ", Name=" +
+                            business[1] + ", Address=" + business[2] + ", City=" +
+                            business[3] + ", State=" + business[4] + "]");
+                    System.out.println("Tiempo para HashFunction1: " + time1 + " ns");
+                    System.out.println("Tiempo para HashFunction2: " + time2 + " ns");
+                    id++;
+                } else {
+                    System.err.println("Saltando línea inválida: " + line);
+                }
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Introduce la Clave a Buscar: ");
+            String searchKey = scanner.nextLine().trim();
+
+            int foundIndex1 = hashTable1.searchAndGetIndex(searchKey);
+            int foundIndex2 = hashTable2.searchAndGetIndex(searchKey);
+
+            if (foundIndex1 != -1 || foundIndex2 != -1) {
+                System.out.println("Clave '" + searchKey + "' encontrada en el índice: " + foundIndex1 + " " + foundIndex2);
+            } else {
+                System.out.println("Clave '" + searchKey + "' no encontrada.");
+            }
+
+
+            System.out.print("Introduce el índice para mostrar datos: ");
+            int searchIndex = scanner.nextInt();
+
+            long startTimeDisplay1 = System.nanoTime();
+            System.out.println("Datos en el índice " + searchIndex + " de la Tabla Hash 1:");
+            List<String> data1 = hashTable1.getDataAtIndex(searchIndex);
+            for (String data : data1) {
+                System.out.println(data);
+            }
+
+            System.out.println("Datos en el índice " + searchIndex + " de la Tabla Hash 2:");
+            List<String> data2 = hashTable2.getDataAtIndex(searchIndex);
+            for (String data : data2) {
+                System.out.println(data);
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
